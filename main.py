76eac81e3f2f359fd5df1b04d1272191c476e46c
Jacob McMichael <jacobmcmichael@gmail.com>
2021-09-15 13:27:41 -0700

    Included functionality for reading directories and listing file contents.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Connecting to an FTP Server
+# ----- Connecting to an FTP Server -----
 # We will connect anonymously to Port 21 - Default FTP Port
 
 # Anonymous FTP Connection
@@ -17,7 +17,7 @@ else:
 
 # Authenticated Login
 # Use an authenticated login to the FTP server using two different methods.
-# 1. Passing in the 'user' and 'passwd' parameters to the 'FTP()' constructor.
+# Method #1: Passing in the 'user' and 'passwd' parameters to the 'FTP()' constructor.
 # Note: I've intentionally passed in the wrong credentials for this example.
 try:
     print("\nMethod #1\n\tAttempting authenticated connection with login details...")
@@ -27,7 +27,7 @@ except Exception as e:
     print(f"\tException... {e}")
 else:
     print("\tSuccess!")
-# 2. Calling 'connect()' and 'login()'.
+# Method #2: Calling 'connect()' and 'login()'.
 try:
     print("\nMethod #2\n\tAttempting authenticated connection with login details...")
     ftp = FTP()
@@ -47,8 +47,45 @@ try:
     print(
         "\nConnection with SSL/TLS\n\tAttempting authenticated connection with login details..."
     )
-    with FTP_TLS("ftp.example.com", "anonymous", "password") as ftp:
+    with FTP_TLS("speedtest.tele2.net", "anonymous", "password") as ftp:
         print(f"\t{ftp.getwelcome()}")
+except Exception as e:
+    print(f"\tException... {e}")
+else:
+    print("\tSuccess!")
+
+# ----- Working with Directories -----
+# Printing the current working directory.
+try:
+    print(
+        "\nConnection with SSL/TLS\n\tAttempting authenticated connection with login details..."
+    )
+    with FTP("ftp.dlptest.com", "dlpuser", "rNrKYTX9g7z3RgJRmxWuGHbeu") as ftp:
+        print(f"\t{ftp.pwd()}")  # Usually the default is /
+
+        try:
+            ftp.mkd("my_dir")  # Make a directory
+        except Exception as e:
+            print(f"\tException... {e}")
+
+        try:
+            ftp.rmd("my_dir")  # Remove a directory
+        except Exception as e:
+            print(f"\tException... {e}")
+
+        try:
+            ftp.cwd("other_dir")  # Change current working directory
+        except Exception as e:
+            print(f"\tException... {e}")
+
+        try:
+            # Listing directory files
+            files = []
+            ftp.dir(files.append)  # Takes a callback for each file
+            for file in files:
+                print(f"\tfile")
+        except Exception as e:
+            print(f"\tException... {e}")
 except Exception as e:
     print(f"\tException... {e}")
 else:
