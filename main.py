432adb1bd1a96c5bc28aaa6c8bf9c48354b5a9ea
Jacob McMichael <jacobmcmichael@gmail.com>
2021-09-23 14:00:21 -0700

    Included methods for file size checking, renaming, and downloading/writing to local copy.

--- main.py
+++ main.py
@@ -5,54 +5,54 @@
 # Use an anonymous login to the FTP server.
 from ftplib import FTP
 
-# try:
-#     print("\nAttempting anonymous login...")
-#     with FTP("speedtest.tele2.net") as ftp:
-#         print(f"\t{ftp.getwelcome()}")
-#         # A "with" statement will automatically close the connection when it reaches the end of the code block.
-# except Exception as e:
-#     print(f"\tException... {e}")
-# else:
-#     print("\tSuccess!")
+try:
+    print("\nAttempting anonymous login...")
+    with FTP("ftp.dlptest.com") as ftp:
+        print(f"\t{ftp.getwelcome()}")
+        # A "with" statement will automatically close the connection when it reaches the end of the code block.
+except Exception as e:
+    print(f"\tException... {e}")
+else:
+    print("\tSuccess!")
 
-# # Authenticated Login
-# # Use an authenticated login to the FTP server using two different methods.
-# # Method #1: Passing in the 'user' and 'passwd' parameters to the 'FTP()' constructor.
-# # Note: I've intentionally passed in the wrong credentials for this example.
-# try:
-#     print("\nMethod #1\n\tAttempting authenticated connection with login details...")
-#     with FTP(host="speedtest.tele2.net", user="user", passwd="password") as ftp:
-#         print(f"\t{ftp.getwelcome()}")
-# except Exception as e:
-#     print(f"\tException... {e}")
-# else:
-#     print("\tSuccess!")
-# # Method #2: Calling 'connect()' and 'login()'.
-# try:
-#     print("\nMethod #2\n\tAttempting authenticated connection with login details...")
-#     ftp = FTP()
-#     ftp.connect("speedtest.tele2.net")
-#     ftp.login("anonymous", "password")
-#     print(f"\t{ftp.getwelcome()}")
-#     ftp.close()
-# except Exception as e:
-#     print(f"\tException... {e}")
-# else:
-#     print("\tSuccess!")
+# Authenticated Login
+# Use an authenticated login to the FTP server using two different methods.
+# Method #1: Passing in the 'user' and 'passwd' parameters to the 'FTP()' constructor.
+# Note: I've intentionally passed in the wrong credentials for this example.
+try:
+    print("\nMethod #1\n\tAttempting authenticated connection with login details...")
+    with FTP(host="ftp.dlptest.com", user="user", passwd="password") as ftp:
+        print(f"\t{ftp.getwelcome()}")
+except Exception as e:
+    print(f"\tException... {e}")
+else:
+    print("\tSuccess!")
+# Method #2: Calling 'connect()' and 'login()'.
+try:
+    print("\nMethod #2\n\tAttempting authenticated connection with login details...")
+    ftp = FTP()
+    ftp.connect("ftp.dlptest.com")
+    ftp.login("dlpuser", "rNrKYTX9g7z3RgJRmxWuGHbeu")
+    print(f"\t{ftp.getwelcome()}")
+    ftp.close()
+except Exception as e:
+    print(f"\tException... {e}")
+else:
+    print("\tSuccess!")
 
-# # Connection with SSL/TLS Over Default Port 21
+# Connection with SSL/TLS Over Default Port 21
 from ftplib import FTP_TLS
 
-# try:
-#     print(
-#         "\nConnection with SSL/TLS\n\tAttempting authenticated connection with login details..."
-#     )
-#     with FTP_TLS("speedtest.tele2.net", "anonymous", "password") as ftp:
-#         print(f"\t{ftp.getwelcome()}")
-# except Exception as e:
-#     print(f"\tException... {e}")
-# else:
-#     print("\tSuccess!")
+try:
+    print(
+        "\nConnection with SSL/TLS\n\tAttempting authenticated connection with login details..."
+    )
+    with FTP_TLS("ftp.dlptest.com", "dlpuser", "rNrKYTX9g7z3RgJRmxWuGHbeu") as ftp:
+        print(f"\t{ftp.getwelcome()}")
+except Exception as e:
+    print(f"\tException... {e}")
+else:
+    print("\tSuccess!")
 
 # ----- Working with Directories -----
 # Printing the current working directory.
@@ -105,7 +105,7 @@ try:
 
         # Upload a test file.
         # For text or binary file, always use `rb`
-        print(f"\tUploading text file...")
+        print(f"\nUploading text file...")
         with open("text.txt", "rb") as text_file:
             ftp.storlines(
                 "STOR text.txt", text_file
@@ -114,11 +114,39 @@ try:
         print(f"\tUploading image file...")
         with open("cat_image.jpg", "rb") as image_file:
             ftp.storbinary("STOR cat_image.jpg", image_file)
-
         print(f"\tSuccessfully uploaded test files.")
 
         print(f"\tGetting a list of new files...")
         for name in ftp.nlst():
             print(f"\t{name}")
+
+        # Get the size of a file.
+        print(f"\nGetting file size...")
+        try:
+            ftp.sendcmd('TYPE I') # 'TYPE I' denotes an image or binary data
+            print(ftp.size('\tImage size in bytes:' + 'cat_image.jpg'))
+        except Exception as e:
+            print(f"\tError checking image size: {e}")
+
+         # Rename a file.
+        print(f"\nRenaming file...")
+        try:
+            ftp.rename('cat_image.jpg', 'cat.jpg') # Change 'cat_image.jpg' to 'cat.jpg'
+            print('\tSuccessfully renamed file!')
+        except Exception as e:
+            print(f"\tError renaming file: {e}")
+
+        # Download a file.
+        print(f"\nDownloading file...")
+        with open("local_text.txt", "w") as local_file: # Open local file for writing
+            response = ftp.retrlines('RETR text.txt', local_file.write)
+            
+            # Check the response code
+            print(f"\tChecking response code...")
+            # https://en.wikipedia.org/wiki/List_of_FTP_server_return_codes
+            if response.startswith('226'):  # Transfer complete
+                print('\tTransfer complete.')
+            else:
+                print('\tError transferring. Local file may be incomplete or corrupt.')
 except Exception as e:
     print(f"\tException... {e}")
